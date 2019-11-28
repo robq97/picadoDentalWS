33fdbea0503f85d53ca1c11ecf9246d45cca9a23
tenox0403 <52614398+tenox0403@users.noreply.github.com>
2019-11-28 03:28:34 -0600

    Método para solicitar lista de citas en orden ascendente

---
+++ PicadoDentalWS/PicadoDentalWS/POCO Models/CitaPOCO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PicadoDentalWS.POCO_Models
+{
+    public class CitaPOCO
+    {
+        public DateTime Fecha { get; set; }
+
+        public String ClienteNombre { get; set; }
+        public String ClienteApellidos{ get; set; }
+        public String DoctorNombre { get; set; }
+        public String DoctorApellidos{ get; set; }
+        public String Detalles { get; set; }
+
+
+    }
+}
